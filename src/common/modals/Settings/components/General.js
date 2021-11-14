00915fc6b5204c873cb169ffdf85b45dce700562
刘郑博 <liuzhengbo@lzbdeMacBook-Pro.local>
2021-11-14 20:03:18 +0800

    fix

--- src/common/modals/Settings/components/General.js
+++ src/common/modals/Settings/components/General.js
@@ -195,7 +195,11 @@ const General = () => {
 
   useEffect(() => {
     ipcRenderer.invoke('getAppVersion').then(setVersion).catch(console.error);
-    extractFace(currentAccount.skin).then(setProfileImage).catch(console.error);
+    if (currentAccount.skin) {
+      extractFace(currentAccount.skin)
+        .then(setProfileImage)
+        .catch(console.error);
+    }
     ipcRenderer
       .invoke('getAppdataPath')
       .then(appData =>
